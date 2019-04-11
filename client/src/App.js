f8230c4112cb942ab221d12fe19ec4d2839eb9dc
Mehdi Vasigh <mehdi.vasigh@arundo.com>
2019-04-11 12:08:39 -0700

    initial commit and client boilerplate

---
+++ client/src/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = () => <div>Hello roshambo</div>;
+
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// CSS reset
+import './styles/reset.css';
+
+ReactDOM.render(<App />, document.getElementById('app'));
