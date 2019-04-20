f62ffb48f252ac705864770311c5ac11a90f7ba3
Mehdi Vasigh <mehdi.vasigh@arundo.com>
2019-04-20 10:44:31 -0500

    add theme

---
+++ client/src/shared/theme.js
@@ -0,0 +1,17 @@
+export const color = {
+  primary: {
+    hue: 338,
+    light: [236, 64, 122],
+    medium: [216, 27, 96],
+    dark: [173, 20, 87]
+  },
+  grayscale: {
+    white: [229, 229, 229],
+    darkGrey: [33, 33, 33],
+    black: [18, 18, 18]
+  }
+};
+
+export const theme = {
+  color: color
+};
